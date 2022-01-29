71b7afcb04875d1451a30d9154741d14698d9aa2
MidasVanVeen <midasvanveen.email@gmail.com>
2022-01-29 14:35:57 +0100

    1

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	char result[1024] = "";
+	char fullmsg[1024] = "";
+	for (int i = 1; i < argc; i++) {
+		strcat(fullmsg, argv[i]);
+		strcat(fullmsg, " ");
+	}
+	char *cat = " /\\_/\\\n( o.o )\n > ^ <";	
+	
+	strcat(result, "\n       ");
+	strcat(result, fullmsg);
+	strcat(result, "\n      /\n");
+	strcat(result, cat);
+
+	printf("%s\n", result);
+	return 0;
+}
